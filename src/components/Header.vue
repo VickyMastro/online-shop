52c2f9ced930536351996a717e4a73ff9ac4d3a5
VickyMastro <vicmastroianni1702@gmail.com>
2022-08-29 12:32:37 -0300

    added component Header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      name: "soy un header",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
