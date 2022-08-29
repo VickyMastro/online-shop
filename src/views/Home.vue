cf6ca1bd9627ca6b574b67be9376c9df3ca47a31
VickyMastro <vicmastroianni1702@gmail.com>
2022-08-29 12:33:30 -0300

    use Header in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <HelloWorld />
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
